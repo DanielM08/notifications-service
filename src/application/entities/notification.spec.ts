ef1d312fde455ebe31843bb8cc07f058a3dceee8
daniel.carvalho <daniel.carvalho@maxmilhas.com.br>
2022-12-14 08:53:09 -0300

    unit tests to use cases

---
+++ src/application/entities/notification.spec.ts
@@ -0,0 +1,22 @@
+import { Notification } from './notification';
+import { Content } from './notification-content';
+
+describe('Notification content', () => {
+  it('should be able to create a notification', () => {
+    const notification = new Notification({
+      content: new Content('Você recebeu uma solicitação de amizade'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notification).toBeTruthy();
+  });
+
+  it('should not be able to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('aaa')).toThrow();
+  });
+
+  it('should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
+});
